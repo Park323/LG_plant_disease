94c03d3a26536610a2ecd4951715a0ac6ebaffa8
JeongKyunPark <park32323@gmail.com>
2022-01-05 16:38:57 +0900

    dataset
    Custom Dataset과
    Train-Valid Seperation을 위한 코드.

---
+++ dataset/dataset.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import cv2, json
+
+import torch
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, files, labels=None, mode='train', label_encoder=None, csv_feature_dict=None):
+        self.mode = mode
+        self.files = files
+        self.csv_feature_dict = csv_feature_dict
+        self.csv_feature_check = [0]*len(self.files)
+        self.csv_features = [None]*len(self.files)
+        self.max_len = -1 * 24*6
+        self.label_encoder = label_encoder
+
+    def __len__(self):
+        return len(self.files)
+    
+    def __getitem__(self, i):
+        file = self.files[i]
+        file_name = file.split('/')[-1]
+        
+        json_path = f'{file}/{file_name}.json'
+        image_path = f'{file}/{file_name}.jpg'
+        
+        if self.csv_feature_check[i] == 0:
+            csv_path = f'{file}/{file_name}.csv'
+            df = pd.read_csv(csv_path)
+
+            # MinMax scaling
+            for col in self.csv_feature_dict.keys():
+                df[col] = df[col] - self.csv_feature_dict[col][0]
+                df[col] = df[col] / (self.csv_feature_dict[col][1]-self.csv_feature_dict[col][0])
+            
+            # transpose to sequential data
+            csv_feature = df[self.csv_feature_dict.keys()].to_numpy()[self.max_len:].T
+            self.csv_features[i] = csv_feature
+            self.csv_feature_check[i] = 1
+        else:
+            csv_feature = self.csv_features[i]
+        
+        img = cv2.imread(image_path)
+        img = cv2.resize(img, dsize=(256, 256), interpolation=cv2.INTER_AREA)
+        img = img.astype(np.float32)/255
+        img = np.transpose(img, (2,0,1))
+        
+        if self.mode == 'train':
+            with open(json_path, 'r') as f:
+                json_file = json.load(f)
+            
+            crop = json_file['annotations']['crop']
+            disease = json_file['annotations']['disease']
+            risk = json_file['annotations']['risk']
+            label = f'{crop}_{disease}_{risk}'
+            
+            return {
+                'img' : torch.tensor(img, dtype=torch.float32),
+                'csv_feature' : torch.tensor(csv_feature, dtype=torch.float32),
+                'label' : torch.tensor(self.label_encoder[label], dtype=torch.long)
+            }
+        else:
+            return {
+                'img' : torch.tensor(img, dtype=torch.float32),
+                'csv_feature' : torch.tensor(csv_feature, dtype=torch.float32)
+            }
---
+++ dataset/tv_seperation.py
@@ -0,0 +1,25 @@
+import yaml
+import os
+import numpy as np
+
+if __name__=='__main__':
+    with open('config/config.yaml') as f:
+        config = yaml.load(f,Loader=yaml.FullLoader)
+    
+    data_dir = config['DATA']['DATA_PATH']
+    val_rate = config['DATA']['VALID_RATE']
+    train_path = config['DATA']['TRAIN_PATH']
+    valid_path = config['DATA']['VALID_PATH']
+    
+    JOIN = lambda x: os.path.join(data_dir, x)
+    files = os.listdir(data_dir)
+    files = [file for file in list(map(JOIN, files)) if os.path.isdir(file)]
+    np.random.shuffle(files)
+    
+    train_files = files[:-int(len(files) * val_rate)]
+    valid_files = files[-int(len(files) * val_rate):]
+    
+    with open(JOIN(train_path), 'w') as f:
+        f.write('\n'.join(train_files))
+    with open(JOIN(valid_path), 'w') as f:
+        f.write('\n'.join(valid_files))
