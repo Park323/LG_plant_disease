238eb1c828184aa84021281c6e4ebcae3fa93182
JeongKyunPark <park32323@gmail.com>
2022-01-12 12:18:26 +0900

    Test.txt 생성파일
    tv_seperation.py가 testset에 대해서도 txt파일을 생성하도록 수정

--- dataset/tv_seperation.py
+++ dataset/tv_seperation.py
@@ -1,7 +1,6 @@
 import yaml
 import os
 import numpy as np
-from omegaconf import OmegaConf
 
 if __name__=='__main__':
     with open('config/config.yaml') as f:
@@ -9,9 +8,11 @@ if __name__=='__main__':
     
     data_dir = config['DATA']['DATA_ROOT']
     img_folder = f"{config['DATA']['DATA_ROOT']}/{config['DATA']['IMAGE_PATH']}"
+    test_folder = f"{config['DATA']['TEST_FOLDER']}"
     val_rate = config['DATA']['VALID_RATE']
     train_path = config['DATA']['TRAIN_PATH']
     valid_path = config['DATA']['VALID_PATH']
+    test_path  = config['DATA']['TEST_PATH']
     
     JOIN = lambda x: img_folder + '/' + x
     files = os.listdir(img_folder)
@@ -21,8 +22,14 @@ if __name__=='__main__':
     train_files = files[:-int(len(files) * val_rate)]
     valid_files = files[-int(len(files) * val_rate):]
     
+    JOIN = lambda x: test_folder + '/' + x
+    test_files = os.listdir(test_folder)
+    test_files = [file for file in list(map(JOIN, files)) if os.path.isdir(file)]
+    
     JOIN = lambda x: data_dir + '/' + x
     with open(JOIN(train_path), 'w') as f:
         f.write('\n'.join(train_files))
     with open(JOIN(valid_path), 'w') as f:
         f.write('\n'.join(valid_files))
+    with open(JOIN(test_path), 'w') as f:
+        f.write('\n'.join(test_files))
